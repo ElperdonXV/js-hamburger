e6c181e63fe34cc549d5134d51fe9e2da8c7ace4
ElperdonXV <davidemartinografico@gmail.com>
2021-11-22 15:58:33 +0100

    all done

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const hamburger = document.querySelector(".header-right > a");
 const navMenu = document.querySelector(".hamburger-menu");
+const hamburgerClose = document.querySelector(".close");
 
 hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+})
+
+hamburgerClose.addEventListener('click', function() {
+    navMenu.classList.remove("active");
 })
